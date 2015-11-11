43cd5607bbaf552d421c60308b7b60f6f017a9dd
Chris DiMartino <vwchris@gmail.com>
2015-11-11 11:09:48 -0500

    Fixed Bird->Pair relationship and added annotations

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,6 +1,16 @@
 class Bird < ActiveRecord::Base
-  has_many :pairs, foreign_key: :player_1_id
-  has_many :eggs, through: :pairs
+  # A bird has_one pair... let's stop this polyamorousness
+  has_one :pair, foreign_key: :player_1_id # We need to tell the association about the FK here - it cannot infer it
+
+  # AR looks at pair.mate to find this.  Since the name of the association is not Bird, we need to tell AR that it's a Bird object by specifying 'class' here
+  has_one :mate, through: :pair, class: Bird
+
+  # This is a straightforward HasManyThrough relationship.  The fact that it is more than one table away does not matter.  AR will determine the path to get there by looking at the Pair association and following its associations
+  has_many :eggs, through: :pair
+
+  # Another has_many through relationship... but the Nest is really a House on the association.
   has_many :nests, through: :eggs, source: :house
+
+  # We've named the foreign key to describe what the relationship is more clearly, so we have to tell it that the class is a House
   belongs_to :primary_house, class: House
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,5 @@
 class House < ActiveRecord::Base
   has_many :eggs
+  # We need to define the foreign_key to make this relationship work
   has_many :birds, foreign_key: :primary_house_id
 end
